3fc36bbb09a2809928dd92b48dc296124c1deef5
Schuberty <schubertyi@gmail.com>
2021-08-15 02:11:32 -0300

    Fixed missing composite ID for locality model

--- src/main/java/io/schuberty/dio/checkpointapi/model/checkpoint/Locality.java
+++ src/main/java/io/schuberty/dio/checkpointapi/model/checkpoint/Locality.java
@@ -1,13 +1,15 @@
 package io.schuberty.dio.checkpointapi.model.checkpoint;
 
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,12 +22,24 @@ import lombok.Setter;
 @Entity
 public class Locality {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @EmbeddedId    
+    private LocalityId id;
 
     @ManyToOne
     private AccessLevel accessLevel;
 
     private String description;
+
+    @Getter
+    @Setter
+    @EqualsAndHashCode
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Embeddable
+    public static class LocalityId implements Serializable {
+
+        private Long localityId;
+        private Long accessLevelId;
+
+    }
 }
