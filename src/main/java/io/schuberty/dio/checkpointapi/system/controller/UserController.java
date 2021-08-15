2b5d7ecce27be49e37a353870d1d87c465a6ffbc
Schuberty <schubertyi@gmail.com>
2021-08-15 02:59:15 -0300

    Updated User endpoints

--- src/main/java/io/schuberty/dio/checkpointapi/system/controller/UserController.java
+++ src/main/java/io/schuberty/dio/checkpointapi/system/controller/UserController.java
@@ -1,13 +1,20 @@
 package io.schuberty.dio.checkpointapi.system.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.schuberty.dio.checkpointapi.exception.UserNotFoundException;
 import io.schuberty.dio.checkpointapi.model.User;
 import io.schuberty.dio.checkpointapi.system.dto.response.MessageResponseDTO;
 import io.schuberty.dio.checkpointapi.system.service.UserService;
@@ -24,5 +31,29 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createUser(@RequestBody User user) {
         return this.userService.create(user);
+    }
+
+    @GetMapping(value = "")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> listAll() {
+        return this.userService.listAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public User findById(@PathVariable("id") Long id) throws UserNotFoundException {
+        return this.userService.findById(id);
+    }
+
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public MessageResponseDTO updateById(@PathVariable("id") Long id, @RequestBody User user) throws UserNotFoundException {
+        return this.userService.updateById(id, user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public MessageResponseDTO deleteById(@PathVariable("id") Long id) throws UserNotFoundException {
+        return this.userService.delete(id);
     }
 }
--- src/main/java/io/schuberty/dio/checkpointapi/system/service/UserService.java
+++ src/main/java/io/schuberty/dio/checkpointapi/system/service/UserService.java
@@ -1,8 +1,11 @@
 package io.schuberty.dio.checkpointapi.system.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.schuberty.dio.checkpointapi.exception.UserNotFoundException;
 import io.schuberty.dio.checkpointapi.model.User;
 import io.schuberty.dio.checkpointapi.system.dto.response.MessageResponseDTO;
 import io.schuberty.dio.checkpointapi.system.repository.UserRepository;
@@ -18,5 +21,31 @@ public class UserService {
     public MessageResponseDTO create(User user) {
         User createdUser = this.userRepository.save(user);
         return MessageResponse.create("Created user with ID " + createdUser.getId());
+    }
+
+    public List<User> listAll() {
+        return this.userRepository.findAll();
+    }
+    
+    public User findById(Long id) throws UserNotFoundException {
+        User user = verifyIfExists(id);
+        return user;
+    }
+
+    public MessageResponseDTO updateById(Long id, User user) throws UserNotFoundException {
+        verifyIfExists(id);
+        User updatedUser = this.userRepository.save(user);
+        return MessageResponse.create("Updated user with ID " + updatedUser.getId());
+    } 
+
+    public MessageResponseDTO delete(Long id) throws UserNotFoundException {
+        verifyIfExists(id);
+        this.userRepository.deleteById(id);
+        return MessageResponse.create("Deleted user with ID " + id);
+    }
+
+    private User verifyIfExists(Long id) throws UserNotFoundException {
+        return this.userRepository.findById(id)
+            .orElseThrow(() -> new UserNotFoundException(id));
     }
 }
