92ae672262af7bc02948d5dfad9fdcf56d99081f
Schuberty <schubertyi@gmail.com>
2021-08-16 01:16:47 -0300

    Add H2 database suport

---
+++ src/main/resources/db/h2/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO access_level VALUES (1, 'Lobby');
+INSERT INTO access_level VALUES (2, 'Garage');
+INSERT INTO access_level VALUES (3, 'Office');
+INSERT INTO access_level VALUES (4, 'Terrace');
+INSERT INTO access_level VALUES (5, 'Basement');
+
+INSERT INTO user_category VALUES (1, 'Visitor');
+INSERT INTO user_category VALUES (2, 'Outsourced');
+INSERT INTO user_category VALUES (3, 'Employed');
+
+INSERT INTO data_type VALUES (1, 'Holiday');
+INSERT INTO data_type VALUES (2, 'Weekend');
